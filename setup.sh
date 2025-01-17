d04997b3881f2d27437ca0a6969dd6a610a9a0ec
Hunter Tunnicliff <hunter@tunnicliff.co>
2025-01-17 08:49:49 -0800

    Adjust setup structure

--- setup.sh
+++ setup.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
-d=$(pwd)
+init_dev_dir() {
+  path="$HOME/Dev"
 
-# shellcheck disable=SC2045
-for file in $(ls -a "$d/home-files"); do
-  if [[ "$file" == "." ]] || [[ "$file" == ".." ]]; then
-    continue
-  fi
-
-  # If the file already exists, ask for confirmation and replace the file
-  if [[ -f "$HOME/$file" ]]; then
-    echo "File $file already exists in $HOME. Replace it? (y/n)"
-    read -r replace
-    if [[ $replace == "y" ]]; then
-     ln -sf "$d/home-files/$file" "$HOME/$file"
-    fi
+  if [[ ! -d "$path" ]]; then
+    echo "Creating dev directory"
+    mkdir "$path"
   else
-    ln -s "$d/home-files/$file" "$HOME/$file"
+    echo "Dev directory already exists"
   fi
-done
+}
 
-if [[ ! -d "$HOME/Dev" ]]; then
-  echo "Creating dev directory"
-  mkdir "$HOME/Dev"
-fi
+init_git_config() {
+  path="$HOME/Dev/.gitconfig"
 
-if [[ ! -f "$HOME/Dev/.gitconfig" ]]; then
-  echo "Creating gitconfig"
-  echo "Enter your preferred git email"
-  read -r email
-  cat <<EOF > "$HOME/Dev/.gitconfig"
-[user]
-  email = "$email"
-EOF
-fi
+  if [[ ! -f "$path" ]]; then
+    echo "Creating gitconfig"
+    echo "Enter your preferred git email"
+    read -r email
+    git config --file "$path" user.email "$email"
+  else
+    echo "Dev gitconfig already exists"
+  fi
+}
+
+init_dotfiles() {
+  home_files=$(ls -a "./home-files")
+
+  for file in $home_files; do
+    echo "$file"
+    continue
+
+    # Skip the current and parent directory
+    if [[ "$file" == "." ]] || [[ "$file" == ".." ]]; then
+      continue
+    fi
+
+    # If the file already exists, ask for confirmation and replace the file
+    if [[ -f "$HOME/$file" ]]; then
+      echo "File $file already exists in $HOME. Replace it? (y/n)"
+      read -r replace
+      if [[ $replace == "y" ]]; then
+      ln -sf "./home-files/$file" "$HOME/$file"
+      else
+        echo "Skipping $file"
+      fi
+    else
+      ln -s "./home-files/$file" "$HOME/$file"
+      echo "Linked $file"
+    fi
+  done
+}
+
+
+init_dotfiles
+init_dev_dir
+init_git_config
